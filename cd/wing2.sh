d5b0125cfe2e1ec94842d73393026066b25a76d4
dharmvahani984-hub <dharmvahani984@gmail.com>
2026-01-17 04:15:08 +0530

    Update wing2.sh

--- cd/wing2.sh
+++ cd/wing2.sh
@@ -250,6 +250,6 @@ fi
 
 echo -e ""
 echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e "${CYAN}           Thank you for using Nobita-hosting!   ${NC}"
+echo -e "${CYAN}           Thank you for using Dharm-hosting!   ${NC}"
 echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 echo -e ""
